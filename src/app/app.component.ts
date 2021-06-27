0167604326d7a26e9a9b1ffd4a46e2611ffd2358
Douglas Martins <martins.douglas1997@outlook.com>
2021-06-27 18:34:21 -0300

    Ajuste no layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { MoviesService } from './movies.service';
 import { Component, OnInit } from '@angular/core';
 
+interface Movie {
+  backdrop: string;
+  id: number;
+  overview: string;
+  poster: string;
+  year: string;
+  title: string;
+  vote_average: number;
+  vote_count: number;
+  genres: string[];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  movies: any;
-  genres: any;
+  movies!: Movie[];
+  moviesFiltered!: Movie[];
+  search: string = '';
 
   constructor(
     private moviesService: MoviesService
@@ -20,5 +33,15 @@ export class AppComponent implements OnInit {
 
   async getMovies() {
     this.movies = await this.moviesService.getMovies();
+    this.moviesFiltered = this.movies;
+  }
+
+  handleSearch() {
+    const search = this.search.toLowerCase().trim();
+
+    this.moviesFiltered = this.movies.filter((movie: any) => {
+      const title = movie.title.toLowerCase().trim();
+      return title.includes(search);
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     HttpClientModule
   ],
