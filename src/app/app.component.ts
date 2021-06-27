d1cfa42b68e5796357a435bcbfa4920c9494b56d
Douglas Martins <martins.douglas1997@outlook.com>
2021-06-26 22:56:40 -0300

    Finalizado layout da lista de filmes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { MoviesService } from './movies.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'movies-list';
+export class AppComponent implements OnInit {
+  movies: any;
+  genres: any;
+
+  constructor(
+    private moviesService: MoviesService
+  ) { }
+
+  async ngOnInit() {
+    this.getMovies();
+  }
+
+  async getMovies() {
+    this.movies = await this.moviesService.getMovies();
+  }
 }
